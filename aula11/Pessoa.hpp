c0228582a273cd27dc47eb00df4efcf2c6b17663
GUILHERME COSTA PATEIRO <gcp19@inf.ufpr.br>
2022-12-07 14:57:00 -0300

    aula11 adicionana

---
+++ aula11/Pessoa.hpp
@@ -0,0 +1,29 @@
+#ifndef PESSOA_H
+#define PESSOA_H
+
+#include<string>
+
+class Pessoa{
+    public:
+        Pessoa();
+        Pessoa(std::string nome);
+        Pessoa(std::string nome, unsigned long cpf, unsigned short int idade);
+
+        unsigned long getCpf() const ;
+        void setCpf(unsigned long cpf);
+
+        std::string getNome() const ;
+        void setNome(std::string nome);
+
+        unsigned short int getIdade() const ;
+        void setIdade(unsigned short int idade);
+
+        void imprimirEnderecoMemoria();
+    private:
+        bool validarCPF(unsigned long cpfTeste);
+
+        std::string nome;
+        unsigned long cpf;
+        unsigned char idade;
+};
+#endif
---
+++ aula11/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+#include <list>
+
+#include "Disciplina.hpp"
+#include "SalaAula.hpp"
+#include "ConteudoMinistrado.hpp"
+
+int main(){
+    int valor = 1330;
+    std::cout<< valor << std::endl;
+}
